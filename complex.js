b7720e96101dad1c15ab6d0c4042db6267453347
Yinka Alabi <adeyinka.alabi.a@gmail.com>
2017-02-21 20:38:24 +0100

    Added the multiply and divide methods to the Complex library

--- complex.js
+++ complex.js
@@ -21,3 +21,26 @@ Complex.prototype.subtract = function(that) {
   var result = new Complex(r, i);
   return result;
 };
+
+Complex.prototype.multiply = function(that) {
+  var r = (this.real * that.real) - (this.imaginary * that.imaginary);
+  var i = (this.real * that.imaginary) + (this.imaginary * that.real);
+  var result = new Complex(r, i);
+  return result;
+};
+
+Complex.prototype.divide = function(that) {
+  if (that.real == 0 && that.imaginary == 0){
+    return "NaN. Cannot divide by 0";
+  }
+  var divisor = Math.pow(that.real, 2) + Math.pow(that.imaginary, 2);
+  var r = ((this.real * that.real) + (this.imaginary * that.imaginary))/divisor;
+  var i = ((this.real * -that.imaginary) + (this.imaginary * that.real))/divisor;
+  var result = new Complex(r, i);
+  return result;
+
+};
+
+module.exports = {
+    Complex : Complex
+}
